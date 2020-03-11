a9ca3dffd0e9b5bc76c5e332070661a59526138a
Maxim <maksfomin@gmail.com>
2020-03-11 22:10:50 +0300

    Lesson 2

---
+++ src/main/java/Lesson2/Netty/common/FileRequest.java
@@ -0,0 +1,13 @@
+package Lesson2.Netty.common;
+
+public class FileRequest extends AbstractMessage {
+    private String filename;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public FileRequest(String filename) {
+        this.filename = filename;
+    }
+}
---
+++ src/main/java/Lesson2/Netty/servers/file_transfer/caching_error/FileRequest.java
@@ -0,0 +1,6 @@
+package Lesson2.Netty.servers.file_transfer.caching_error;
+
+import java.io.Serializable;
+
+public class FileRequest implements Serializable {
+}
