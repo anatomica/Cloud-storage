12eb1ee0b0426572e671050ae517ac68787d0c19
Maxim <maksfomin@gmail.com>
2020-03-23 22:15:08 +0300

    Homework 4 step 12 (Delete files, what don't need)

--- src/main/java/Lesson2/Netty/common/AbstractMessage.java
+++
@@ -1,6 +0,0 @@
-package Lesson2.Netty.common;
-
-import java.io.Serializable;
-
-public abstract class AbstractMessage implements Serializable {
-}
--- src/main/java/Lesson2/Netty/servers/file_transfer/caching_error/FileRequest.java
+++
@@ -1,6 +0,0 @@
-package Lesson2.Netty.servers.file_transfer.caching_error;
-
-import java.io.Serializable;
-
-public class FileRequest implements Serializable {
-}
